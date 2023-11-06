f2001fde050e2b3663621fe30b2c2987cb684cd5
Frederico Brion <fredericoasbrion@gmail.com>
2023-11-06 14:54:39 -0300

    feat: cria a model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+        references: {
+          model: 'BlogPost',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+        references: {
+          model: 'Category',
+          key: 'id',
+        },
+      },
+    },
+    {
+      timestamps: false,
+      tableName: 'posts_categories',
+      underscored: true,
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
